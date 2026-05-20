c79dde3f5870dcbcab13c088ec0de87175007497
David <dmpuerto23@gmail.com>
2026-05-20 20:07:56 -0300

    genero clase Empresa e implemento primeros dos metodos

--- src/ar/edu/ungs/billetera/Billetera.java
+++ src/ar/edu/ungs/billetera/Billetera.java
@@ -1,20 +1,45 @@
 package ar.edu.ungs.billetera;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class Billetera implements IBilletera {
 
+	// ATRIBUTOS
+	private HashMap<String,Empresa> diccEmpresasPorCuit;
+	
+	
+	// CONSTRUCTOR. INICIALIZO VARIABLES
+	public Billetera() {
+		this.diccEmpresasPorCuit = new HashMap<>();
+	}
+	
+	
+	// METODOS PUBLICOS:
 	@Override
-	public void registrarEmpresa(String cuit, String nombreFantasia, String telefono, String email,
-			String nombreContacto) {
-		// TODO Auto-generated method stub
+	public void registrarEmpresa(String cuit, String nombreFantasia, String telefono, String email, String nombreContacto) {
+		// Lanza error si la empresa ya esta registrada o algun campo es inválido.
+		validarCamposRegistrarEmpresa(cuit, nombreFantasia, telefono, email, nombreContacto);
 
+		Empresa empresa = new Empresa(cuit,nombreFantasia,telefono,email,nombreContacto);
+				
+		diccEmpresasPorCuit.put(cuit, empresa);
 	}
 
 	@Override
 	public void agregarPersonaAutorizada(String cuitEmpresa, String dniAutorizado) {
-		// TODO Auto-generated method stub
-
+		// Lanza error si la empresa no existe
+		if(!diccEmpresasPorCuit.containsKey(cuitEmpresa))
+			throw new IllegalArgumentException("la empresa no existe");
+		
+		Empresa empresa = diccEmpresasPorCuit.get(cuitEmpresa);
+		
+		// Lanza error si la persona ya está autorizada.
+		if (empresa.contienePersonaAutorizada(dniAutorizado))
+			throw new IllegalArgumentException("la persona ya esta autorizada");
+		
+		empresa.agregarPersonaAutorizada(dniAutorizado);
+		
 	}
 
 	@Override
@@ -120,4 +145,33 @@ public class Billetera implements IBilletera {
 		return null;
 	}
 
+
+	
+	// METODOS PRIVADOS
+	/* 
+	 * Lanza error si la empresa ya esta registrada o algun campo es inválido.
+	 * */
+	// TODO: validaciones correctas? se puede validar que ademas telefono sea un numero, que nombre no sea un numero etc
+	private void validarCamposRegistrarEmpresa(String cuit, String nombreFantasia, String telefono, String email, String nombreContacto) {
+		
+		if(diccEmpresasPorCuit.containsKey(cuit))
+			throw new IllegalArgumentException("la empresa ya esta registrada");
+		
+		if (nombreFantasia == null || nombreFantasia.trim().isEmpty()) 
+	        throw new IllegalArgumentException("El nombre de fantasia no puede ser vacio o no tener caracteres");
+	    
+	    if (telefono == null || telefono.trim().isEmpty()) 
+	        throw new IllegalArgumentException("El telefono no puede ser vacio o no tener caracteres");
+	    
+	    if (email == null || email.trim().isEmpty()) 
+	        throw new IllegalArgumentException("El email no puede ser vacio o no tener caracteres");
+	    
+	    if (!email.contains("@")) 
+	        throw new IllegalArgumentException("El email cargado debe tener el caracter @");
+	    
+	    if (nombreContacto == null || nombreContacto.trim().isEmpty()) 
+	        throw new IllegalArgumentException("El nombre de contacto no puede ser vacio o no tener caracteres");
+	    
+	}
+	
 }
---
+++ src/ar/edu/ungs/billetera/Empresa.java
@@ -0,0 +1,34 @@
+package ar.edu.ungs.billetera;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Empresa {
+	
+	private String cuit;
+	private String nombreFantasia; 
+	private String telefono;
+	private String email;
+	private String nombreContacto;
+	private Set<String> dnisPersonasAutorizadas;
+	
+	public Empresa(String cuit, String nombreFantasia, String telefono, String email, String nombreContacto) {
+		this.cuit = cuit;
+		this.nombreFantasia = nombreFantasia; 
+		this.telefono = telefono;
+		this.email = email;
+		this.nombreContacto = nombreContacto;
+		this.dnisPersonasAutorizadas = new HashSet<>();
+	}
+
+	public void agregarPersonaAutorizada(String dniAutorizado) {
+		this.dnisPersonasAutorizadas.add(dniAutorizado);
+	}
+
+	public boolean contienePersonaAutorizada(String dniAutorizado) {
+		return dnisPersonasAutorizadas.contains(dniAutorizado);
+	}
+	
+	
+	
+}
